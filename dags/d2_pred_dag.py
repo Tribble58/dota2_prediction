9ef20edc78aa425b743934ebdbac45c1e4625433
Vladlen <vladlensakhnyuk@yandex.ru>
2024-07-03 21:50:20 +0300

    Added 3 tasks that create job, extract data and insert it in database

--- dags/d2_pred_dag.py
+++ dags/d2_pred_dag.py
@@ -12,8 +12,8 @@ from airflow.models import Variable
 
 default_args = {
     "owner": "Vladlen",
-    "retries": 5,
-    "retry_delay": timedelta(minutes=5)
+    "retries": 50,
+    "retry_delay": timedelta(seconds=30)
 }
 
 
@@ -24,195 +24,207 @@ def read_file(variable):
     return file
 
 
-def execute_query(query):
+def execute_query(query, fetch=True):
     pg_hook = PostgresHook(
-        postgres_conn_id='d2_dwh',
-        schema='d2_dwh'
+        postgres_conn_id='d2_dwh'
     )
     pg_conn = pg_hook.get_conn()
     cursor = pg_conn.cursor()
     cursor.execute(query)
-    returned_value = cursor.fetchall()
-    pg_conn.commit()
+    if fetch:
+        returned_value = cursor.fetchall()
+        pg_conn.commit()
+    else:
+        pg_conn.commit()
+        return
     return returned_value
 
 
 def initialize_job(ti):
+    """
+    DAG TASK
+    Creates a new job in service.jobs table and pushes its uuid to the XCom
+    :param ti: XCom argument
+    """
     try:
-        etl = read_file('etl_path')
-        print("ETL-file has been loaded successfully")
-        etl = json.dumps(etl)
-        etl = str(etl).replace("'", "''")
+        job_code = 'dota2_etl'
         print("Creating a new job...")
-        query = f"select service.create_job('{etl}');"
+        query = f"select service.create_job('{job_code}');"
         job_uid = execute_query(query)[0][0]
-        print("Job has been created successfully")
+        print("Job has been created successfully!")
         ti.xcom_push(key='job_uid', value=job_uid)
     except BaseException as e:
         print(f"ERROR: {e}")
 
 
+def query_insert_package(etl_json_item, data):
+    """
+    Creates a new package in service.packages table with the given JSON
+    :param etl_json_item: header of result JSON
+    :param data: data that was extracted from the source
+    """
+    etl_json_item['data'] = data
+
+    etl_json_item = json.dumps(etl_json_item)
+    etl_json_item = str(etl_json_item).replace("'", "''")
+    query = f"select service.create_package('{etl_json_item}');"
+    execute_query(query)
+    print("Package has been created successfully!")
+
+
+def request_data(url):
+    """
+    Requests data based on the provided URL. If response is not successful, try again after a delay. Raise an exception
+    if number of tries exceeds count limit
+    :param url: Query string of request
+    :return: JSON of the response
+    """
+    print("Requesting data...")
+    count = 0
+    while count < 10:
+        response = requests.request("GET", url)
+        if response.status_code != 200:
+            print(f"ERROR! Response code is {response.status_code}, response message: {response.text}")
+            count += 1
+            time.sleep(5)
+            continue
+        print("Data has been retrieved successfully!")
+        return json.loads(response.content)
+    raise ValueError("ERROR requesting data")
+
+
 def extract_data(ti):
+    """
+    DAG TASK
+    Extracts data from API and inserts packages to service.packages
+    :param ti: XCom argument
+    """
     priority = read_file('priority_path')
     job_uid = ti.xcom_pull(task_ids='initialize_job', key='job_uid')
-    query = f"select etl from service.jobs where uid = '{job_uid}'"
-    etl = execute_query(query)[0][0]
-    try:
-        package_size = int(Variable.get('package_size'))
-        print(f"Package size: {package_size}")
-    except Exception as e:
-        print(f"Error getting package size: {e.message}")
-    etl['meta']['data_package_size'] = package_size
-    etl['meta']['job_uid'] = job_uid
-    for item in priority:
-        etl_item = deepcopy(etl)
-        # etl['stage'] = 'extract'
-        table_name = item['table_name']
-        url = item['url']
-        # etl['start_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        # print(etl['meta']['table_name'])
-        etl_item['meta']['table_name'] = table_name
-        if '<id>' not in url:
-            if table_name == 'teams':
-                page = 0
-                data = []
-                while True:  # TODO: доделать, не совсем нравится
-                    url = item['url']
-                    etl_item_task = deepcopy(etl_item)
-                    url += f'?page={page}'
-                    for k in range(3):
-                        response = requests.request("GET", url)
-                        if response.status_code == 200:
-                            etl_item_task['logs'][f"Try: {k + 1}"] = "Data was extracted successfully"
-                            break
-                        else:
-                            etl_item_task['logs'][
-                                f"Try: {k + 1}"] = f"Status code of response is {response.status_code}"
-                            time.sleep(5)
-                    page_data = json.loads(response.content)
-                    if isinstance(page_data, dict):
-                        time.sleep(3)
-                        continue
-                    if len(page_data) != 0:
-                        etl_item_task['data'] = page_data
-                        etl_item_task = json.dumps(etl_item_task)
-                        etl_item_task = str(etl_item_task).replace("'", "''")
-                        query = f"select service.create_package('{etl_item_task}');"
-                        execute_query(query)
-                        page += 1
-                        time.sleep(3)
-                    else:
-                        break
-                continue
 
-            for k in range(3):
-                response = requests.request("GET", url)
-                if response.status_code == 200:
-                    data = json.loads(response.content)
-                    etl_item['logs'][f"Try: {k + 1}"] = "Data was extracted successfully"
-                    break
-                else:
-                    etl_item['logs'][f"Try: {k + 1}"] = f"Status code of response is {response.status_code}"
-                    time.sleep(5)
-        else:
-            # TODO: Create script that processes url with <ids>
-            matches_data = requests.request("GET", 'https://api.opendota.com/api/proMatches')
-            for match in matches_data:
-                match_id = match['id']
-                for k in range(3):
-                    response = requests.request("GET", url.replace('<id>', id))
-                    if response.status_code == 200:
-                        match_data = json.loads(response.content)
-                        etl_item['logs'][f"Try: {k + 1}"] = "Data was extracted successfully"
-                        break
-                    else:
-                        etl_item['logs'][f"Try: {k + 1}"] = f"Status code of response is {response.status_code}"
-                        time.sleep(5)
-                data.append(match_data)
-            etl_item['logs']["Try: 1"] = "Data was extracted successfully"
+    # Size of package that data shpuld be split on
+    package_size = int(Variable.get('package_size'))
+    print(f"Package size: {package_size}")
 
-        etl_item['data'] = data
-        # etl['end_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # Iterate over priority file and extract each entity
+    for temp_item in priority:
+        url = temp_item['url']
+        table_name = temp_item['table_name']
+        print("-" * 50)
+        print(f'Table name: {table_name}')
+        print("-" * 50)
 
-        etl_item = json.dumps(etl_item)
-        etl_item = str(etl_item).replace("'", "''")
-        query = f"select service.create_package('{etl_item}');"
-        execute_query(query)
-
-
-def transform_data(ti):
-    mapping = read_file('mapping_path')
-    job_uid = ti.xcom_pull(task_ids='initialize_job', key='job_uid')
-    query = f"select p.table_name, p.uid from service.packages p " \
-            f"left join service.jobs j on p.job_id = j.id " \
-            f"where j.uid = '{job_uid}';"
-    packages_info = execute_query(query)
-
-    default_values = ['', '-', '—']
-    for table_name, package_uid in packages_info:
-        query = f"select package from service.packages where uid = '{package_uid}'"
-        [[data]] = execute_query(query)
-        data_transformed = []
-        for table_mapping in mapping:
-            if table_mapping['table_name_source'] == table_name:
-                for row in data:
-                    object = {
-                        'table_name_dwh': table_mapping['table_name_dwh'],
-                        'unique_field': table_mapping['unique_field']
-                    }
-                    for field_source, field_dwh in table_mapping['fields'].items():
-                        value = row[field_source].replace('\"', "'") if isinstance(row[field_source], str) else row[
-                            field_source]
-                        if table_mapping['transformations'][field_source] is not None:
-                            to_eval = table_mapping['transformations'][field_source].replace('<value>', str(value))
-                            value = eval(to_eval)
-                        object[field_dwh] = value if value not in default_values else None
-                    data_transformed.append(object)
-        etl = {
-            "stage": "transform",
+        etl_json = {
+            "data": None,
+            "logs": None,
             "meta": {
-                "table_name": table_name,
-                "package_uid": package_uid
-            },
-            "data": data_transformed,
-            "logs": {"Try: 0": "Data was successfully transformed"}
+                "data_package_size": package_size,
+                "job_uid": job_uid,
+                "table_name": table_name
+            }
         }
-        etl = json.dumps(etl)
-        etl = str(etl).replace("'", "''")
-        query = f"select service.insert_transformed_object('{etl}');"
-        execute_query(query)
+
+        if table_name in ('matches', 'pro_players_heroes', 'heroes_matchups'):
+            # If the table name matches the one in the list below, then it contains a parameter in query string,
+            # thus we need to extract the arguments for these parameters first
+            if table_name == 'matches':
+                ids_url = 'https://api.opendota.com/api/proMatches'
+            elif table_name == 'pro_players_heroes':
+                ids_url = 'https://api.opendota.com/api/proPlayers'
+            else:
+                ids_url = 'https://api.opendota.com/api/heroes'
+
+            print(f"Getting ids for extracting {table_name}")
+
+            ids_data = request_data(ids_url)
+            data = []
+            for id_data in ids_data:
+                if table_name == 'matches':
+                    item_id = id_data['match_id']
+                    data.append(request_data(url.replace('<id>', str(item_id))))
+                else:
+                    if table_name == 'pro_players_heroes':
+                        item_id = id_data['account_id']
+                        temp_data = request_data(url.replace('<id>', str(item_id)))
+                        result = []
+                        for temp_item in temp_data:
+                            temp_dict = deepcopy(temp_item)
+                            temp_dict['account_id'] = item_id
+                            result.append(temp_dict)
+                    else:
+                        item_id = id_data['id']
+                        result = request_data(url.replace('<id>', str(item_id)))
+                        if table_name == 'heroes_matchups':
+                            for hero_matchup in result:
+                                hero_matchup['hero_against_id'] = item_id
+                                # list(map(lambda x: x.update({'hero_against_id': item_id}), result))
+                    data.extend(result)
+                    time.sleep(1)
+            # Insert package to service.packages
+            query_insert_package(etl_json, data)
+        else:
+            if table_name == 'teams':
+                # Request that has a pagination
+                page = 0
+                while page is not None:
+                    print(f'Page number is {page}')
+                    etl_json_page = deepcopy(etl_json)
+                    page_url = f'{url}?page={page}'
+                    page_data = request_data(page_url)
+                    # If data is not empty, insert page to service.packages, break when final package is reached
+                    if len(page_data) > 0:
+                        query_insert_package(etl_json_page, page_data)
+                        page += 1
+                    else:
+                        print('Final page is reached!')
+                        page = None
+            else:
+                data = request_data(url)
+                query_insert_package(etl_json, data)
+
+        print(f"Successfully processed {table_name}")
 
 
 def insert_data(ti):
-    priority = read_file('priority_path')
+    # TODO: replace with autogeneration based on mapping table
+    # Get job uid
     job_uid = ti.xcom_pull(task_ids='initialize_job', key='job_uid')
-    query = f"select p.id from service.packages p " \
-            f"left join service.jobs j on p.job_id = j.id " \
-            f"where j.uid = '{job_uid}';"
-    package_ids = execute_query(query)
-    for table in priority:
-        table_name = table['table_name']
-        for (package_id,) in package_ids:
-            query = f"select object from service.objects where package_id = {package_id} and table_name = '{table_name}';"
-            data = execute_query(query)
-            etl = {
-                "stage": "insert",
-                "meta": {
-                    "table_name": table_name
-                },
-                "data": data,
-                "logs": {"Try: 0": "Data was successfully sent to insert"}
-            }
-            etl = json.dumps(etl)
-            etl = str(etl).replace("'", "''''")
-            query = f"select data.insert_data('{etl}');"
-            print(execute_query(query))
+    priority = read_file('priority_path')
+
+    for priority_item in priority:
+        table_name = priority_item['table_name']
+
+        print("-" * 50)
+        print(f'Table name: {table_name}')
+        print("-" * 50)
+
+        # Get all packages' uids that correspond to this job and table name
+        query = f"select jsonb_agg(p.uid) from service.packages p " \
+                f"left join service.jobs j on p.job_id = j.id " \
+                f"where j.uid = '{job_uid}' and p.table_name = '{table_name}';"
+
+        package_uids = execute_query(query)[0][0]
+        print(f'Package uids: {package_uids}')
+
+        # Iterate over all package uids and insert them
+        count = 1
+        for package_uid in package_uids:
+            print("-" * 50)
+            print(f'Processing {count} of total {len(package_uids)}')
+            print("-" * 50)
+            query = f"call service.insert_data('{table_name}', '{package_uid}');"
+            print(f'Query: {query}')
+            execute_query(query, False)
+
+            count += 1
+
+        print('Data was inserted successfully!')
 
 
 with DAG(
         dag_id='D2_ETL',
         default_args=default_args,
-        start_date=datetime(2022, 10, 28, 1),
+        start_date=datetime.now(),
         schedule_interval='@daily'
 ) as dag:
     initialize_job = PythonOperator(
@@ -224,13 +236,10 @@ with DAG(
         task_id='extract_data',
         python_callable=extract_data
     )
-    transform_data = PythonOperator(
-        task_id='transform_data',
-        python_callable=transform_data
-    )
+
     insert_data = PythonOperator(
         task_id='insert_data',
         python_callable=insert_data
     )
 
-    initialize_job >> extract_data >> transform_data >> insert_data
+    initialize_job >> extract_data >> insert_data
